2f963f3ddc75b66312abf1ed7004028ba2ca2832
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-08-27 08:27:15 +0200

    test du script sur une page d'annuaure startup fr, adaptation du script à venir si inefficace

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from cgitb import html
 from bs4 import BeautifulSoup
 
-with open('home.html', 'r') as html_file:
+with open('annuairestartupmarseill.html', 'r') as html_file:
     content = html_file.read()
 
     soup = BeautifulSoup(content, "html.parser")
